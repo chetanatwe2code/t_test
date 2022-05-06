cb42ca593ca99ce26c97e8d26ce0ac7f7fa358f3
chetanatwe2code <105039844+chetanatwe2code@users.noreply.github.com>
2022-05-06 12:46:07 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter one',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+ 
 }
 
 class MyHomePage extends StatefulWidget {
